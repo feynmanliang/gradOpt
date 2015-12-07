4c41fd7a8790aa290c3244287ed3d933ae882912
Feynman Liang <feynman.liang@gmail.com>
2015-12-07 16:59:40 +0000

    Decrease initial bracket interval length

--- src/main/scala/com/feynmanliang/gradopt/Optimizer.scala
+++ src/main/scala/com/feynmanliang/gradopt/Optimizer.scala
@@ -100,7 +100,7 @@ class Optimizer {
       }
     }
 
-    val initBracket = BracketInterval(x0 - 0.001D, x0, x0 + 0.01D) // TODO: better initial bracket?
+    val initBracket = BracketInterval(x0 - 0.01D, x0, x0 + 0.01D) // TODO: better initial bracket?
 
     doubleBounds(initBracket)
     .take(5000) // stops after 5000 brackets, TODO: more robust stopping criterion
