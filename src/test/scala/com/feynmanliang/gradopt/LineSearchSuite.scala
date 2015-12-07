c3f318ddacf7e0de7cb9deacf0ef4d10d06ada3d
Feynman Liang <feynman.liang@gmail.com>
2015-12-07 23:58:26 +0000

    Splits tests

---
+++ src/test/scala/com/feynmanliang/gradopt/LineSearchSuite.scala
@@ -0,0 +1,35 @@
+package com.feynmanliang.gradopt
+
+import breeze.linalg._
+import breeze.numerics._
+
+import org.scalatest._
+
+
+class LineSearchSuite extends FunSpec {
+  val opt = new Optimizer()
+
+  describe("Line search") {
+    describe("when applied to f(x) = x^2") {
+      val f: Vector[Double] => Double = v => v dot v
+      val df: Vector[Double] => Vector[Double] = x => {
+        require(x.length == 1, "df only defined R -> R")
+        2D*x
+      }
+
+      for (x <- List(-17D, 0D, 4D).map(DenseVector(_))) {
+        val bracket = opt.bracket(f, df, x).get // safe, know x^2 is convex
+        val xnew = opt.lineSearch(f, df, x, bracket)
+        describe(s"when initialized with x=${x}") {
+          it("should return a point within the bracket") {
+            val p = if (norm(df(x).toDenseVector) == 0D) 0D else norm((x - xnew).toDenseVector) / norm(df(x).toDenseVector)
+            assert(bracket.contains(p))
+          }
+          it("should not increase f(x)") {
+            assert(f(xnew) <= f(x))
+          }
+        }
+      }
+    }
+  }
+}
