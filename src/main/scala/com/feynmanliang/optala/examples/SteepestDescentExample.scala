b07c5a5c2959e573d31a5dfd3c77c7414a5cf945
Feynman Liang <feynman.liang@gmail.com>
2015-12-14 20:33:40 +0000

    Format output to latex table

--- src/main/scala/com/feynmanliang/optala/examples/SteepestDescentExample.scala
+++ src/main/scala/com/feynmanliang/optala/examples/SteepestDescentExample.scala
@@ -1,36 +1,26 @@
 package com.feynmanliang.optala.examples
 
-import breeze.linalg.linspace
+import breeze.linalg.DenseMatrix
 import breeze.numerics.{cos, pow, sin}
-import breeze.plot.{Figure, plot}
-
 import com.feynmanliang.optala.GradientAlgorithm._
 import com.feynmanliang.optala.GradientOptimizer
 import com.feynmanliang.optala.LineSearchConfig._
 
 object SteepestDescentExample {
   def main(args: Array[String]) {
-    val showPlot = args(0).toBoolean
-
     val f = (x: Double) => pow(x, 4) * cos(pow(x, -1)) + 2D * pow(x, 4)
     val df = (x: Double) => 8D * pow(x, 3) + 4D * pow(x, 3) * cos(pow(x, -1)) - pow(x, 4) * sin(pow(x, -1))
 
-    if (showPlot) {
-      val fig = Figure()
-      val x = linspace(-.1, 0.1)
-      fig.subplot(2, 1, 0) += plot(x, x.map(f))
-      fig.subplot(2, 1, 1) += plot(x, x.map(df))
-      fig.saveas("lines.png") // save current figure as a .png, eps and pdf also supported
-    }
-
-    val gradOpt = new GradientOptimizer(maxSteps = 5000, tol = 1E-6)
-    for (x0 <- List[Double](-5, -1, -0.1, -1E-2, -1E-3, -1E-4, -1E-5, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1, 5)) {
+    val gradOpt = new GradientOptimizer(maxSteps = 10000, tol = 1E-8)
+    for {
+      x0 <- List[Double](-50, -10, -5, -1, -0.1, -1E-2, -1E-3, -1E-4, -1E-5, 1E-5, 1E-4, 1E-3, 1E-2, 0.1, 1, 5, 10, 50)
+    } {
       gradOpt.minimize(f, df, x0, SteepestDescent, CubicInterpolation, reportPerf = true) match {
         case (_, Some(perf)) =>
-          val (xstar, fstar) = perf.stateTrace.last
-          println(s"x0:$x0,xstar:$xstar,fstar:$fstar,normGrad:${perf.stateTrace.last._2}," +
-            s"numSteps:${perf.stateTrace.length},fEval:${perf.numObjEval},dfEval:${perf.numGradEval}")
-        case _ => println(s"No results for x0=$x0!!!")
+          println(f"$x0 & ${perf.stateTrace.size} & ${perf.numObjEval} & ${perf.numGradEval}" +
+            f" & ${perf.stateTrace.last._1(0)}%.3E & ${perf.stateTrace.last._2}%.3E\\\\")
+          DenseMatrix(x0 +: perf.stateTrace.flatMap(_._1.toArray):_*)
+        case _ => sys.error(s"No results for x0=$x0!!!")
       }
     }
   }
