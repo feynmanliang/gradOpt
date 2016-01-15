9764846c4e03781071d165748f4de6e0b4e1d80e
Feynman Liang <feynman.liang@gmail.com>
2016-01-15 08:51:40 +0000

    Final work before submission

--- src/main/scala/com/feynmanliang/optala/examples/QuadraticFormExample.scala
+++ src/main/scala/com/feynmanliang/optala/examples/QuadraticFormExample.scala
@@ -13,32 +13,29 @@ import com.feynmanliang.optala.GradientAlgorithm._
 import com.feynmanliang.optala.LineSearchConfig._
 
 object QuadraticFormExample {
-  val SEED = 42L
+  val SEED = 45L
   implicit val rand = new RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister(SEED)))
 
   def main(args: Array[String]) {
-    val gradOpt = new GradientOptimizer(maxSteps = 100000, tol = 1E-4)
+    val gradOpt = new GradientOptimizer(maxSteps = 100000, tol = 1E-3)
     for {
+      fname <- List("A10", "A100", "A1000", "B10", "B100", "B1000")
+      a = csvread(new File(args(0) + fname + ".csv"))
+      n = a.cols
+      b = DenseVector.fill(n) { Uniform(-1D,1D)(rand).sample() }
       lsAlgo <- List(Exact, CubicInterpolation)
+      optAlgo <- List(ConjugateGradient, SteepestDescent)
     } yield {
-      println(s"====$lsAlgo=====")
-      for {
-        //fname <- List("A10", "A100", "A1000", "B10", "B100", "B1000")
-        fname <- List("B10", "B100", "A10", "A100", "B1000", "A1000")
-        a = csvread(new File(getClass.getResource("/" + fname + ".csv").getFile))
-        n = a.cols
-        b = DenseVector.fill(n) { Uniform(-1D,1D)(rand).sample() }
-        optAlgo <- List(SteepestDescent, ConjugateGradient)
-      } yield {
-        assert(a.rows == a.cols, "A must be symmetric")
+      assert(a.rows == a.cols, "A must be symmetric")
 
-        gradOpt.minQuadraticForm(a, b, DenseVector.zeros(n), optAlgo, lsAlgo) match {
-          case Success(results) =>
-            println(s"$optAlgo & $fname & ${results.stateTrace.size} & ${results.numObjEval} & ${results.numGradEval}" +
-              f" & ${results.bestSolution.objVal}%.3E & ${results.bestSolution.normGrad}%.3E\\\\")
-          case Failure(e) => println(e.getMessage)
-        }
+      println(s"====$lsAlgo=====")
+      gradOpt.minQuadraticForm(a, b, DenseVector.zeros(n), optAlgo, lsAlgo) match {
+        case Success(results) =>
+          println(s"$optAlgo & $fname & ${results.stateTrace.size} & ${results.numObjEval} & ${results.numGradEval}" +
+            f" & ${results.bestSolution.objVal}%.3E & ${results.bestSolution.normGrad}%.3E\\\\")
+        case Failure(e) => println(e.getMessage)
       }
     }
   }
 }
+
